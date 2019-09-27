d38c3bd884bd838493fc4e638e88391ba2d484c5
kalpeshsignzy <kalpesh@signzy.com>
2019-09-28 00:25:14 +0530

    Make dynamic classes

---
+++ app/src/main/java/com/example/calculator/DynamicCalculator.java
@@ -0,0 +1,13 @@
+package com.example.calculator;
+
+public class DynamicCalculator {
+
+    public static double add(double d1, double d2) {
+        return d1 + d2;
+    }
+
+    public static double subtract(double d1, double d2) {
+        return d2 - d1;
+    }
+
+}
